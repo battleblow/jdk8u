49729b263e02c70594bdc5f4e83b9418baf6aafc
Kurt Miller <bsdkurt@gmail.com>
2023-12-15 13:53:28 -0500

    Fix passing NULL to non-pointer warnings on sparc64

--- hotspot/src/cpu/sparc/vm/c1_CodeStubs_sparc.cpp
+++ hotspot/src/cpu/sparc/vm/c1_CodeStubs_sparc.cpp
@@ -297,7 +297,7 @@ void PatchingStub::emit_code(LIR_Assembler* ce) {
 #ifdef ASSERT
     address start = __ pc();
 #endif
-    AddressLiteral addrlit(NULL, metadata_Relocation::spec(_index));
+    AddressLiteral addrlit((intptr_t)NULL, metadata_Relocation::spec(_index));
     __ patchable_set(addrlit, _obj);
 
 #ifdef ASSERT
@@ -312,7 +312,7 @@ void PatchingStub::emit_code(LIR_Assembler* ce) {
 #ifdef ASSERT
     address start = __ pc();
 #endif
-    AddressLiteral addrlit(NULL, oop_Relocation::spec(_index));
+    AddressLiteral addrlit((intptr_t)NULL, oop_Relocation::spec(_index));
     __ patchable_set(addrlit, _obj);
 
 #ifdef ASSERT

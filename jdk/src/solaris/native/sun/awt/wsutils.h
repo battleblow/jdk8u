4dba1ba5e350dfb3020760a413b368059264b178
Greg Lewis <glewis@eyesbeyond.com>
2024-07-27 11:28:39 -0700

    Get definition from standard header

--- jdk/src/solaris/native/sun/awt/wsutils.h
+++ jdk/src/solaris/native/sun/awt/wsutils.h
@@ -70,7 +70,7 @@ from The Open Group.
  *
  ******************************************************************************/
 
-typedef unsigned long Pixel;
+#include <X11/Intrinsic.h> /* For Pixel */
 
 /* This is the actual structure returned by the X server describing the
  * SERVER_OVERLAY_VISUAL property.
